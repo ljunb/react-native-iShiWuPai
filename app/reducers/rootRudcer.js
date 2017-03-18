80759159129227c68685708bcd96a996cf38b617
CookieJ <l_junb@163.com>
2017-03-18 22:21:26 +0800

    移除多余文件

--- app/reducers/rootRudcer.js
+++ app/reducers/rootRudcer.js
@@ -3,22 +3,14 @@
  * 根reducer
  */
 import { combineReducers } from 'redux';
-import FoodsList from './foodsListReducer';
 import FoodCompare from './foodCompareReducer';
 import FoodInfo from './foodInfoReducer';
 import Search from './searchReducer';
 import feedHome from './feed/feedHomeListReducer';
-import feedDelicacy from './feed/feedDelicacyListReducer';
-import feedEvaluating from './feed/feedEvaluatingListReducer';
-import feedKnowledge from './feed/feedKnowledgeListReducer';
 
 export default rootReducer = combineReducers({
-    FoodsList,
     FoodCompare,
     FoodInfo,
     Search,
     feedHome,
-    feedDelicacy,
-    feedEvaluating,
-    feedKnowledge
 })
