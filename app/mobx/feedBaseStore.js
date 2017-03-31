633d798614b616e1910a202322126b0b4e4ca393
BigLoafer <soso_hb_zx@163.com>
2017-03-31 09:57:51 +0800

    没有数据的时候 bug

--- app/mobx/feedBaseStore.js
+++ app/mobx/feedBaseStore.js
@@ -2,32 +2,38 @@
  * Created by ljunb on 2017/02/28.
  */
 import {observable, computed, action, runInAction} from 'mobx'
-
 export default class FeedStore {
-    @observable feedList = []
-    @observable errorMsg = ''
-    @observable page = 1
-    @observable isRefreshing = false
+    @observable feedList = [];
+    @observable errorMsg = '';
+    @observable page = 1;
+    @observable isRefreshing = false;
+    @observable flag=true;
 
     constructor(categoryId) {
-        this.categoryId = categoryId
+        this.categoryId = categoryId;
         this.fetchFeedList()
     }
 
-    @action
+
+   @action
     fetchFeedList = async () => {
         try {
             if (this.isRefreshing) this.page = 1
 
-            const result = await this._fetchDataFromUrl()
+            const result = await this._fetchDataFromUrl();
             runInAction(() => {
                 this.isRefreshing = false
                 this.errorMsg = ''
 
+                if(result.length<10) this.flag=false;
+                else this.flag=true;
+                
                 if (this.page == 1) {
                     this.feedList.replace(result)
                 } else {
                     this.feedList.splice(this.feedList.length, 0, ...result);
+                    //this.feedList.push(...result);
+                    //this.feedList=this.feedList.concat(result);
                 }
             })
         } catch (error) {
@@ -50,8 +56,8 @@ export default class FeedStore {
             const URL = `http://food.boohee.com/fb/v1/feeds/category_feed?page=${this.page}&category=${this.categoryId}&per=10`
 
             fetch(URL).then(response => {
-                if (response.status == 200) return response.json()
-                return null
+                if (response.status == 200) return response.json();
+                return null;
             }).then(responseData => {
                 if (responseData) {
                     resolve(responseData.feeds)
